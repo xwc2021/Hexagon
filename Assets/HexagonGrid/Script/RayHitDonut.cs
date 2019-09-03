39a0819ce960a21b63ecfd48046347ddc8bdb2a4
xiangwei71 <xiangwei71@gmail.com>
2019-09-03 22:05:10 +0800

    更新Hexagon.shadergraph
    點擊會顯示不同的顏色

---
+++ Assets/HexagonGrid/Script/RayHitDonut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayHitDonut : MonoBehaviour
+{
+    public UpdateDountMaterial updateDountMaterial;
+    Camera using_camera;
+    private void Awake()
+    {
+        using_camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = using_camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                var uv = hit.textureCoord;
+                Debug.Log(uv);
+                updateDountMaterial.updateMaterialProperty(uv);
+            }
+            else
+                Debug.Log("not Hit");
+
+
+        }
+
+    }
+}
---
+++ Assets/HexagonGrid/Script/UpdateDountMaterial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateDountMaterial : MonoBehaviour
+{
+    public Material material;
+    // Update is called once per frame
+    public void updateMaterialProperty(Vector2 uv)
+    {
+        material.SetVector("_userClickUV", uv);
+    }
+}
